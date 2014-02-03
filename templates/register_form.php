fc46bebbe99fdf5b703c956c7e36c2b07f8038ee
n2iw <yingming18@gmail.com>
2014-02-03 01:08:34 -0500

    Fix register_form keyboard focus

--- templates/register_form.php
+++ templates/register_form.php
@@ -4,19 +4,19 @@
             <input autofocus name="call" placeholder="Call sign" type="text"/>
         </div>
         <div class="control-group">
-            <input autofocus name="name" placeholder="Name" type="text"/>
+            <input name="name" placeholder="Name" type="text"/>
         </div>
         <div class="control-group">
-            <input autofocus name="email" placeholder="Email" type="text"/>
+            <input name="email" placeholder="Email" type="text"/>
         </div>
         <div class="control-group">
-            <input autofocus name="phone" placeholder="Phone" type="text"/>
+            <input name="phone" placeholder="Phone" type="text"/>
         </div>
         <div class="control-group">
             <input name="password" placeholder="Password" type="password"/>
         </div>
         <div class="control-group">
-            <input name="confirmation" placeholder="Confirmation" type="password"/>
+            <input name="confirmation" placeholder="Password again" type="password"/>
         </div>
         <div class="control-group">
             <button type="submit" class="btn">Register</button>
