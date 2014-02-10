630eb66f42f79b898043771c0f44f5ee8f189614
n2iw <yingming18@gmail.com>
2014-02-10 00:00:38 -0500

    Add linkedin link to footer

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
             </div>
 
             <div id="bottom">
-                Copyright &#169; James Ying N2IW
+                Copyright &#169; <a target="_blank" href="http://www.linkedin.com/pub/ming-ying/76/b5a/ba8">James Ying N2IW</a> 2014
             </div>
 
         </div>
