16d4562a4b21ec24692655c443efefe086dd64ae
n2iw <yingming18@gmail.com>
2014-02-19 23:50:50 -0500

    Change copyright link to n2iw.com/about.php

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
             </div>
 
             <div id="bottom">
-                Copyright &#169; <a target="_blank" href="http://www.linkedin.com/pub/ming-ying/76/b5a/ba8">James Ying N2IW</a> 2014
+                Copyright &#169; <a target="_blank" href="http://n2iw.com/about.php">James Ying N2IW</a> 2014
             </div>
 
         </div>
