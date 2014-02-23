0f05d929b4bde2151f09a8f8eea3bf4200cff851
n2iw <yingming18@gmail.com>
2014-02-23 01:35:43 -0500

    Add about author link on footer

--- templates/footer.php
+++ templates/footer.php
@@ -3,6 +3,7 @@
 
             <div id="bottom">
                 Copyright &#169; <a target="_blank" href="http://n2iw.com/about.php">James Ying N2IW</a> 2014
+                <a href="http://n2iw.com/about.php" target="_blank">About Author</a>
             </div>
 
         </div>
